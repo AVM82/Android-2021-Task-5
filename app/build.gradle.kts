6f90e340f6b623cb92d72a294550a12d5eba3f33
avm82 <1982alexvm@gmail.com>
2021-09-27 12:29:27 +0300

    add glide dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi:1.11.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
-//    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
