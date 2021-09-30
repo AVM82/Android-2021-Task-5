40b1d56ea98ab35af7c3e477ebc7233b4be61c6b
avm82 <1982alexvm@gmail.com>
2021-09-30 23:46:04 +0300

    try to save image to gallery

--- app/src/main/java/org/rsschool/android2021task5/ui/detail/DetailFragment.kt
+++ app/src/main/java/org/rsschool/android2021task5/ui/detail/DetailFragment.kt
@@ -1,6 +1,13 @@
 package org.rsschool.android2021task5.ui.detail
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +19,10 @@ import org.rsschool.android2021task5.R
 import org.rsschool.android2021task5.databinding.DetailFragmentBinding
 import org.rsschool.android2021task5.helper.getDefaultRequestOptions
 import org.rsschool.android2021task5.model.ImageDTO
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 private const val ARG_IMAGE = "item"
 
@@ -36,6 +47,17 @@ class DetailFragment : Fragment() {
         image = arguments?.getParcelable(ARG_IMAGE)
         views {
             toolBar.setNavigationOnClickListener { parentFragmentManager.popBackStack() }
+            saveButton.setOnClickListener {
+
+                var bitmap =
+                    Bitmap.createBitmap(detailImage.width, detailImage.height, Bitmap.Config.ARGB_8888)
+                var canvas = Canvas(bitmap)
+                detailImage.draw(canvas)
+
+                saveMediaToStorage(bitmap)
+
+
+            }
             image?.let {
                 widthValueText.text = getString(R.string.height, it.width.toString())
                 heightValueText.text = getString(R.string.width, it.height.toString())
@@ -47,6 +69,36 @@ class DetailFragment : Fragment() {
                     ).into(detailImage)
             } ?: detailImage.setImageResource(R.drawable.ic_baseline_cloud_download_24)
         }
+    }
+
+
+    //the function I already explained, it is used to save the Bitmap to external storage
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        val filename = "${System.currentTimeMillis()}.gif"
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            context?.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/gif")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+//        fos?.use {
+        val stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+        fos?.write(stream.toByteArray())
+//        }
     }
 
     companion object {
