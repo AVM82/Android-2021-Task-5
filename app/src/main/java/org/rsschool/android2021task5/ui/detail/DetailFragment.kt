737dd9a0607f93a9a46069e1b8b3c9eccd9e2fdd
avm82 <1982alexvm@gmail.com>
2021-10-04 22:54:11 +0300

    implement save image to gallery

--- app/src/main/java/org/rsschool/android2021task5/ui/detail/DetailFragment.kt
+++ app/src/main/java/org/rsschool/android2021task5/ui/detail/DetailFragment.kt
@@ -1,51 +1,32 @@
 package org.rsschool.android2021task5.ui.detail
 
 import android.Manifest
-import android.app.DownloadManager
-import android.content.ContentValues
-import android.content.Context.DOWNLOAD_SERVICE
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
+import android.media.MediaScannerConnection
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import org.rsschool.android2021task5.R
 import org.rsschool.android2021task5.databinding.DetailFragmentBinding
-import org.rsschool.android2021task5.helper.PermissionHelper
 import org.rsschool.android2021task5.helper.getDefaultRequestOptions
 import org.rsschool.android2021task5.model.ImageDTO
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
-import java.nio.ByteBuffer
 
 private const val ARG_IMAGE = "item"
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
-    interface PermissionListener {
-        fun onGranted(isGranted: Boolean)
-    }
-
     private val binding get() = requireNotNull(_binding)
-    private val requestPermissionLauncher = PermissionHelper().
-
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission(), this::onGranted)
+    private var detailFragmentPresenter: DetailFragmentPresenter? = null
     private var image: ImageDTO? = null
     private var _binding: DetailFragmentBinding? = null
 
@@ -58,50 +39,20 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
-    private fun saveIMG(detailImage: ImageView) {
-        when (detailImage.drawable) {
-            is GifDrawable -> saveGif((detailImage.drawable as GifDrawable).buffer)
-            is BitmapDrawable -> saveBitmap((detailImage.drawable as BitmapDrawable).bitmap)
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        requestPermissionLauncher.launch(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        detailFragmentPresenter = context?.let { DetailFragmentPresenter(it.contentResolver) }
         image = arguments?.getParcelable(ARG_IMAGE)
         views {
             toolBar.setNavigationOnClickListener { parentFragmentManager.popBackStack() }
             saveButton.setOnClickListener {
-
-                    PermissionHelper().permissionRequest(this@DetailFragment) {
-                        saveIMG(detailImage)
-                    }
-
-
-//                when (PackageManager.PERMISSION_GRANTED) {
-//                    ContextCompat.checkSelfPermission(
-//                        requireContext(),
-//                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-//                    ) -> {
-//                        saveIMG()
-//                    }
-//                    else -> {
-//                        // You can directly ask for the permission.
-//                        // The registered ActivityResultCallback gets the result of this request.
-//                        requestPermissionLauncher.launch(
-//                            Manifest.permission.WRITE_EXTERNAL_STORAGE
-//                        )
-//                    }
-//                }
-
-
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                } else {
+                    saveImage()
+                }
             }
             image?.let {
                 widthValueText.text = getString(R.string.height, it.width.toString())
@@ -113,85 +64,6 @@ class DetailFragment : Fragment() {
                         getDefaultRequestOptions()
                     ).into(detailImage)
             } ?: detailImage.setImageResource(R.drawable.ic_baseline_cloud_download_24)
-        }
-    }
-
-    private fun saveGif(buffer: ByteBuffer) {
-        val filename = "${System.currentTimeMillis()}.gif"
-        val imagesDir =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val gifFile = File(imagesDir, filename)
-
-        val output = FileOutputStream(gifFile)
-        val bytes = ByteArray(buffer.capacity())
-        (buffer.duplicate().clear() as ByteBuffer).get(bytes)
-        output.write(bytes, 0, bytes.size)
-        output.close()
-    }
-
-
-
-    private fun save(image: ImageDTO?) {
-        val request = DownloadManager.Request(Uri.parse(image?.url))
-            .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-            .setTitle("Voyager")
-            .setDescription("File is downloading...")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setAllowedOverMetered(true)
-            .setAllowedOverRoaming(false)
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "fileName")
-        val downloadManager = activity?.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        val downloadID = downloadManager.enqueue(request)
-
-    }
-
-
-//    fun isStoragePermissionGranted(): Boolean {
-//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (requireActivity().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                == PackageManager.PERMISSION_GRANTED
-//            ) {
-//                //Permission is granted
-//                true
-//            } else {
-//                //Permission is revoked
-//                ActivityCompat.requestPermissions(
-//                    requireActivity(),
-//                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-//                    1
-//                )
-//                false
-//            }
-//        } else {
-//            //permission is automatically granted on sdk<23 upon installation
-//            //Permission is granted
-//            true
-//        }
-//    }
-
-
-    private fun saveBitmap(bitmap: Bitmap) {
-        val filename = "${System.currentTimeMillis()}.gif"
-        var fos: OutputStream? = null
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            context?.contentResolver?.also { resolver ->
-                val contentValues = ContentValues().apply {
-                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-                    put(MediaStore.MediaColumns.MIME_TYPE, "image/gif")
-                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-                }
-                val imageUri: Uri? =
-                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-                fos = imageUri?.let { resolver.openOutputStream(it) }
-            }
-        } else {
-            val imagesDir =
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-            val image = File(imagesDir, filename)
-            fos = FileOutputStream(image)
-        }
-        fos?.use {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
         }
     }
 
@@ -211,4 +83,26 @@ class DetailFragment : Fragment() {
     }
 
     private fun <T> views(block: DetailFragmentBinding.() -> T) = binding.block()
+
+    private fun onGranted(isGranted: Boolean) {
+        views {
+            if (isGranted) {
+                saveImage()
+            } else {
+                val snackBar = Snackbar.make(container, "Permission denied", Snackbar.LENGTH_LONG)
+                snackBar.show()
+            }
+        }
+    }
+
+    private fun DetailFragmentBinding.saveImage() {
+        val url = image?.url
+        val filename = url?.subSequence(url.lastIndexOf("/") + 1, url.length)
+            ?: "${System.currentTimeMillis()}.jpg"
+        val path = detailFragmentPresenter?.saveIMG(detailImage.drawable, filename as String)
+        MediaScannerConnection.scanFile(
+            context, arrayOf(path),
+            null, null
+        )
+    }
 }
